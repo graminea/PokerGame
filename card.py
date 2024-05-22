6c9535a8ae4ad3f38d71b07327d6361e7a00db63
graminea <gabrielsagasdasilva@gmail.com>
2024-05-22 20:06:34 -0300

    pokertest1

---
+++ card.py
@@ -0,0 +1,11 @@
+class Card:
+    suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+    value_dict = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    values = list(value_dict.keys())
+
+    def __init__(self, suit, value):
+        self.suit = suit
+        self.value = value
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
---
+++ deck.py
@@ -0,0 +1,10 @@
+import random
+from card import Card
+
+class Deck:
+    def __init__(self):
+        self.cards = [Card(suit, value) for suit in Card.suits for value in Card.values]
+        random.shuffle(self.cards)
+
+    def deal(self):
+        return self.cards.pop()
